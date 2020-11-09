899543cbdd416fc86f859e35fe86583ba0a2c2cb
aditistar15 <aditistar15@gmail.com>
2020-11-09 17:33:55 +0530

    added smoke

--- sketch.js
+++ sketch.js
@@ -30,7 +30,7 @@ function setup(){
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
     pig3 = new Pig(810, 220);
-
+    
     log3 =  new Log(810,180,300, PI/2);
 
     box5 = new Box(810,160,70,70);
@@ -44,6 +44,7 @@ function setup(){
 }
 
 function draw(){
+   // background(0);
     background(backgroundImg);
     Engine.update(engine);
     //strokeWeight(4);
